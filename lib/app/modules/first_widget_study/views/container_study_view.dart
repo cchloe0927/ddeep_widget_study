b78cf1d0800fdd3a234ee173c53796d04843a386
hyunjeong <cchloe0927@gmail.com>
2023-05-08 23:21:17 +0900

    feat : Container Widget

--- lib/app/modules/first_widget_study/views/container_study_view.dart
+++ lib/app/modules/first_widget_study/views/container_study_view.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_study/app/common/widgets/app_bar/study_page_app_bar.dart';
 import 'package:flutter_widget_study/app/modules/first_widget_study/controllers/first_widget_study_controller.dart';
 import 'package:flutter_widget_study/constants/const_app_color.dart';
-import 'package:flutter_widget_study/constants/const_app_size.dart';
 import 'package:flutter_widget_study/constants/const_app_text.dart';
 import 'package:get/get.dart';
 
 class ContainerStudyView extends GetView<FirstWidgetStudyController> {
   const ContainerStudyView({super.key});
-  /// DDEEPWIDGET : DDEEP(위치에 복붙하고 지우기)
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,9 +17,34 @@ class ContainerStudyView extends GetView<FirstWidgetStudyController> {
           appBar: const StudyPageAppBar(
             appBarTitleText: ConstAppText.TEXT_FIRST_CONTAINER,
           ),
+
+          /// DDEEPWIDGET : Container
+          // 자식 위젯을 포함하며 크기를 지정할 수 있음. 자식 위젯이 없거나 크기자 지정되지 않으면 width, height 최댓값으로 고정됌
+          body: Container(
+            color: Colors.red, //color 색상 지정
+            width: 300, //너비
+            height: 300, //높이
+            alignment: Alignment.center, // 컨테이너 안에 자식 요소 가운데 정렬
+            //padding: const EdgeInsets.all(100), //안쪽 여백
+
+            // decoration 안에서 color 지정하니깐 오류 발생
+            // decoration: BoxDecoration(
+            //   color: Colors.blue,
+            //   border: Border.all(
+            //     width: 10,
+            //   ),
+            // ),
+
+            child: const Text(
+              '컨테이너',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
