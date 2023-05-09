b5b43c0c9bbae8aa567e3f7f63de327d554a8896
hyunjeong <cchloe0927@gmail.com>
2023-05-09 15:53:52 +0900

    feat : Container_decoration  설명 추가

--- lib/app/modules/first_widget_study/views/container_study_view.dart
+++ lib/app/modules/first_widget_study/views/container_study_view.dart
@@ -21,19 +21,19 @@ class ContainerStudyView extends GetView<FirstWidgetStudyController> {
           /// DDEEPWIDGET : Container
           // 자식 위젯을 포함하며 크기를 지정할 수 있음. 자식 위젯이 없거나 크기자 지정되지 않으면 width, height 최댓값으로 고정됌
           body: Container(
-            color: Colors.red, //color 색상 지정
+            //color: Colors.red, //color 색상 지정
             width: 300, //너비
             height: 300, //높이
             alignment: Alignment.center, // 컨테이너 안에 자식 요소 가운데 정렬
             //padding: const EdgeInsets.all(100), //안쪽 여백
 
-            // decoration 안에서 color 지정하니깐 오류 발생
-            // decoration: BoxDecoration(
-            //   color: Colors.blue,
-            //   border: Border.all(
-            //     width: 10,
-            //   ),
-            // ),
+            //decoration 안에서 color 사용 시에는 Container_color 또는 decoration_color 둘 중 하나만 사용 가능
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(
+                width: 10,
+              ),
+            ),
 
             child: const Text(
               '컨테이너',
